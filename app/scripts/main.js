9e2f5145111444983521021d85b6eaf1cd212caa
PurplishCode <darellpetualang@gmail.com>
2024-03-31 13:47:12 -0700

    gulp build

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,12 @@
-console.log('\'Allo \'Allo!');
 
 $('.menu_trigger').click(function(){
-  console.log("Clicked");
+  console.log('Clicked');
 // Within the header element, points towards the .menu class, then the ul element inside of it.
-  $('header .menu  ul').addClass('d-block');
+  $('header .menu  ul').addClass('d-block active');
 });
+
+$('.menu_close_trigger').click(function(){
+$('header .menu ul').removeClass('active');
+});
+
+
